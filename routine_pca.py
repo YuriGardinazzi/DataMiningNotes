676f0ae0c76c5de3a3a0278160da50b4b289c85a
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2020-11-18 16:00:16 +0100

    Initial commit

---
+++ routine_pca.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 15:55:51 2020
+
+@author: Yuri
+"""
+
+
+
+if __name__ == '__main__':
+    print("ROUTINE PCA\n \
+          Inseirsci il nome del file Excel da leggere: ")
+    filename = input("file: ")
+    print(f"Il file che vuoi utilizzare si chiama: {filename}")
